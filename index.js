43dfb0cc447e4d64c1a244b12c875a83d85d877c
Asad Ullah <shaikhasad183@gmail.com>
2023-04-18 08:45:36 -0700

    last

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require('./db')
-
+require('dotenv').config()
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = "https://blue-swallow-sock.cyclic.app/"
+const PORT = process.env.PORT || 3000;
+
+
 
 app.use(express.json())
 app.use(cors())
@@ -13,12 +15,17 @@ app.use('/api/notes',require('./routes/notes'))
 app.get('/v1/login', (req, res) => {
     res.send('Hello login!')
   })
+app.get('/',async(req,res)=>{
+
+  res.send('Welcome to INOTEBOOK')
+})
+
 
   app.get('/v1/signup', (req, res) => {
     res.send('Hello Signup!')
   }) 
-app.listen(port, () => {
-    console.log(`Example app listening on localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on localhost:${PORT}`)
 
 })
 
